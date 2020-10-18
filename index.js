b0ecf4e503a05caf400f5abf38fe4d9ccfc7732e
Aaron Vegu <aaron@reckoner-rde.io>
2020-10-18 18:13:56 -0500

    Practice of the logger, that illustrates the usage and mechanics of events emitters and listeners on a more practical example

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-// Importamos nuestro modulo Person y lo definimos como Person
-const Person = require('./Person');
+// // Importamos nuestro modulo Person y lo definimos como Person
+// const Person = require('./Person');
 
-// Instanciamos un objeto de Person
-const johnDoe = new Person('John Doe', 30);
+// // Instanciamos un objeto de Person
+// const johnDoe = new Person('John Doe', 30);
 
-// Usamos su metodo greeting
-johnDoe.greeting();
+// // Usamos su metodo greeting
+// johnDoe.greeting();
+
+// ---------------------
+
+/**
+ * Lo que haremos es crear un listener que estara en escucha del evento 'message', y cuando lo escuche,
+ * nos hara un console.log que imprimira el mensaje emitido por nuestro evento emisor en Logger y el id
+ * que se le asigno desde Logger, el cual emite el evento y un mensaje recibido junto al ID cuando se le
+ * llama al metodo log de Logger que recibe un mensaje como parametro
+ */
+
+// Traemos nuestro modulo Logger
+const Logger = require('./logger');
+
+// Instanciamos nuestro Logger
+const logger = new Logger();
+
+// Creamos un event listener de message
+logger.on('message', (data) => {
+  console.log('Called Listener', data);
+});
+
+// Emitimos nuestro evento mediante el metodo log() de Logger
+logger.log('Hello World!');
